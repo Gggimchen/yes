f716afd0c26ddcf056583b756050ceb75d417977
Digas <43025791+Gggimchen@users.noreply.github.com>
2024-03-23 14:34:55 +0700

    Add files via upload

--- a/app.py
+++ a/app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+
+conn = sqlite3.connect('questions.db', check_same_thread=False)
+conn.row_factory=sqlite3.Row
+cursor=conn.cursor()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods =['GET', 'POST'])
 def SignInPage():
+    if request.method == 'POST':
+        print('POST')
+        username = request.form['username']
+        password = request.form['password']
+        print("signing in ... usernam:"+username+", password=="+password)
+
+        query= "SELECT * FROM users WHERE username=='" + username + "' AND password=='" +password+"'"
+        cursor.execute(query)
+
+        users = cursor.fetchall()
+        if(len(users) > 0):
+            print("Sign in successful")
+            return redirect(url_for('ProductPage'))
+        else:
+            print("No user found")
+
+
     return render_template("sumthing.html")
 
 @app.route("/productpage")
@@ -18,7 +39,14 @@ def ProductPage():
     rows=cursor.fetchall()
     print("Rows:", rows)
 
-    return render_template("productpage.html", rows = rows)
+    return render_template('productpage.html', rows=rows)
+
+@app.route("/productpage/delete/<id>")
+def deleteEntry(id):
+    query = 'DELETE FROM product WHERE id ==' +id
+    cursor.execute(query)
+    conn.commit()
+    return redirect(url_for('ProductPage'))
 
 @app.route("/categories")
 def CategoryPage():
@@ -32,3 +60,16 @@ def CategoryPage():
     print("Rows:", rows)
 
     return render_template("categories.html", rows = rows)
+
+@app.route("/users")
+def UserPage():
+    conn = sqlite3.connect('questions.db')
+    conn.row_factory=sqlite3.Row
+
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM users")
+
+    rows=cursor.fetchall()
+    print("Rows:", rows)
+
+    return render_template("users.html", rows = rows)
